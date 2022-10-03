83cff3fddbf209755775f6c8b762f19c51aa9018
Asit <Asit@DESKTOP-T3GITSD>
2022-10-03 14:41:16 +0530

    first commit by Asit

---
+++ src/test/java/newmavenprojectpackage/AsitClassTest.java
@@ -0,0 +1,10 @@
+package newmavenprojectpackage;
+
+import org.testng.annotations.Test;
+
+public class AsitClassTest {
+	@Test
+	public void GreetingsTest() {
+		System.out.println("Good afternoon Tejas");
+	}
+}
