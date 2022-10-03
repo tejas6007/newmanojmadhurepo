f388da655eb8b6d5ce1c3050c4d206b4e236259f
DELL <DELL@DESKTOP-LILEBQG>
2022-10-03 14:20:45 +0530

    first commit

---
+++ src/test/java/newmavenprojectpackage/newclass.java
@@ -0,0 +1,14 @@
+package newmavenprojectpackage;
+
+import org.testng.annotations.Test;
+
+public class newclass 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("i am tejas ");
+	}
+	}
+
+
